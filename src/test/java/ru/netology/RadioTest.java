da22dc25eff66eef00348d9525bb68b7cd8f0e88
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-06-22 22:58:29 +0400

    modified classes Radio and RadioTest

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -2,228 +2,84 @@ package ru.netology;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RadioTest {
 
-    @Test
-    public void setCurrentStationMinimumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(0);
+    Radio radio = new Radio();
 
-        int expected = 0;
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/setCurrentStationDefault.csv")
+    public void setCurrentStationDefault(int station, int expected) {
+        radio.setCurrentStation(station);
         int actual = radio.getCurrentStation();
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void setCurrentStationBelowMinimumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(-1);
-
-        int expected = 0;
-        int actual = radio.getCurrentStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentStationAboveMinimumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(1);
-
-        int expected = 1;
-        int actual = radio.getCurrentStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentStationMaximumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
-
-        int expected = 9;
-        int actual = radio.getCurrentStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentStationBellowMaximumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(8);
-
-        int expected = 8;
-        int actual = radio.getCurrentStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentStationAboveMaximumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentStation(10);
-
-        int expected = 9;
-        int actual = radio.getCurrentStation();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-
-    @Test
-    public void setCurrentVolumeMinimumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(0);
-
-        int expected = 0;
-        int actual = radio.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentVolumeBellowMinimumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(-1);
-
-        int expected = 0;
-        int actual = radio.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentVolumeAboveMinimumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(1);
-
-        int expected = 1;
-        int actual = radio.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentVolumeMaximumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(10);
-
-        int expected = 10;
-        int actual = radio.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentVolumeBellowMaximumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(9);
-
-        int expected = 9;
-        int actual = radio.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void setCurrentVolumeAboveMaximumValue() {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(11);
-
-        int expected = 10;
-        int actual = radio.getCurrentVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void nextStationMaximumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentStation(9);
-
-        int expected = 0;
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/nextStationDefault.csv")
+    public void nextStationDefault(int station, int expected) {
+        radio.setCurrentStation(station);
         int actual = radio.next();
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void nextStationBellowMaximumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentStation(8);
-
-        int expected = 9;
-        int actual = radio.next();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void prevStationMinimumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentStation(0);
-
-        int expected = 9;
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/prevStationDefault.csv")
+    public void prevStationDefault(int station, int expected) {
+        radio.setCurrentStation(station);
         int actual = radio.prev();
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void prevStationAboveMinimumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentStation(1);
-
-        int expected = 0;
-        int actual = radio.prev();
-
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/setCurrentVolume.csv")
+    public void setCurrentVolume(int volume, int expected) {
+        radio.setCurrentVolume(volume);
+        int actual = radio.getCurrentVolume();
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void upVolumeMaximumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(10);
-
-        int expected = 10;
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/upVolume.csv")
+    public void upVolume(int volume, int expected) {
+        radio.setCurrentVolume(volume);
         int actual = radio.upVolume();
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void upVolumeBellowMaximumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(9);
-
-        int expected = 10;
-        int actual = radio.upVolume();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    public void downVolumeMinimumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(0);
-
-        int expected = 0;
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/downVolume.csv")
+    public void downVolume(int volume, int expected) {
+        radio.setCurrentVolume(volume);
         int actual = radio.downVolume();
-
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void downVolumeAboveMinimumValue () {
-        Radio radio = new Radio();
-        radio.setCurrentVolume(1);
+    Radio radioIsChanged = new Radio(25);
 
-        int expected = 0;
-        int actual = radio.downVolume();
-
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/setCurrentStationIsСhanged.csv")
+    public void setCurrentStationIsСhanged(int station, int expected) {
+        radioIsChanged.setCurrentStation(station);
+        int actual = radioIsChanged.getCurrentStation();
         Assertions.assertEquals(expected, actual);
     }
 
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/nextStationIsСhanged.csv")
+    public void nextStationIsСhanged(int station, int expected) {
+        radioIsChanged.setCurrentStation(station);
+        int actual = radioIsChanged.next();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/prevStationIsСhanged.csv")
+    public void prevStationIsСhanged(int station, int expected) {
+        radioIsChanged.setCurrentStation(station);
+        int actual = radioIsChanged.prev();
+        Assertions.assertEquals(expected, actual);
+    }
 }
